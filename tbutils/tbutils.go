1b6df9661fd91f4d464a431b7eb39fd3d175db4d
Alex Jackson <alex@alexj.org>
2014-10-04 15:59:25 +0100

    Added ability to draw coloured text in tbutils

--- tbutils/tbutils.go
+++ tbutils/tbutils.go
@@ -10,23 +10,38 @@ import (
 // DrawText take a string and adds it to the termbox's back buffer.
 // If the string contains any newline characters, the string will be split across multiple rows.
 // If the string is too long to fit in the termbox, it'll be split across multiple lines to fit.
+// This function is the equivalent of calling DrawRichText(x,y,text,termbox.ColorDefault,termbox.ColorDefault)
 func DrawText(x, y int, text string) {
+	DrawRichText(x, y, text, termbox.ColorDefault, termbox.ColorDefault)
+}
+
+// DrawRichText takes a string and adds it to the termbox's back buffer.
+// If the string contains any newline characters, it'll be split across multiple rows.
+// If the string is too long to find in the termbox, it'll be split across multiple lines to fit.
+func DrawRichText(x, y int, text string, fg, bg termbox.Attribute) {
 	for row, line := range strings.Split(text, "\n") {
-		DrawLine(x, y+row, line)
+		DrawRichLine(x, y+row, line, fg, bg)
 	}
 }
 
 // DrawLine adds a single line of text to the termbox's back buffer.
 // If the line is too long to fit in the termbox, it will be split across multiple lines.
+// This function is the equivalent of calling DrawRichLine(x,y,text,termbox.ColorDefault,termbox.ColorDefault)
 func DrawLine(x, y int, text string) {
+	DrawRichLine(x, y, text, termbox.ColorDefault, termbox.ColorDefault)
+}
+
+// DrawRichLine adds a single line of text to the termbox's back buffer.
+// If the line is too long to fit in the termbox, it will be split across multiple lines.
+func DrawRichLine(x, y int, text string, fg, bg termbox.Attribute) {
 	w, _ := termbox.Size()
 
 	if len(text) > (w - x) {
-		DrawLine(x, y, text[0:(w-x)])
-		DrawLine(x, y+1, text[(w-x):len(text)-1])
+		DrawRichLine(x, y, text[0:(w-x)], fg, bg)
+		DrawRichLine(x, y+1, text[(w-x):len(text)], fg, bg)
 	} else {
-		for index, char := range text {
-			termbox.SetCell(x+index, y, char, termbox.ColorDefault, termbox.ColorDefault)
+		for index, ch := range text {
+			termbox.SetCell(x+index, y, ch, fg, bg)
 		}
 	}
 }
